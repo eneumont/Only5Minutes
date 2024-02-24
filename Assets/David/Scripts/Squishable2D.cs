2bbab842e18af8f1c62777ade95ee360227c8e1b
Zeadrasil <thewitepheonix@gmail.com>
2024-02-24 15:04:59 -0700

    Update Squishable2D.cs
    minor squishable improvement

--- Assets/David/Scripts/Squishable2D.cs
+++ Assets/David/Scripts/Squishable2D.cs
@@ -12,6 +12,7 @@ public class Squishable2D : MonoBehaviour
     private List<Vector2> collisionPoints = new List<Vector2>();
     private List<Collider2D> colliders = new List<Collider2D>();
     private bool damaged = false;
+    private float timer = 0;
     [SerializeField] bool utiliseSpeed;
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,7 @@ public class Squishable2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
         for(int i = 0; i < collisionPoints.Count - 1; i++)
         {
             for(int j = i + 1; j < collisionPoints.Count; j++)
@@ -30,7 +32,7 @@ public class Squishable2D : MonoBehaviour
                 float a1 = Vector2.SignedAngle(Vector2.up, collisionPoints[i] - new Vector2(transform.position.x, transform.position.y));
                 float a2 = Vector2.SignedAngle(Vector2.up, collisionPoints[j] - new Vector2(transform.position.x, transform.position.y));
                 bool b = gameObject.TryGetComponent(out IDamagable subject);
-				if ( a && Mathf.Abs(a1 + a2) > 180 - maximumSquishAngle && b)
+				if ( a && Mathf.Abs(a1 + a2) > 180 - maximumSquishAngle && b && timer <= 0)
                 {
                     float multiplier = 1;
                     if(utiliseSpeed)
@@ -39,6 +41,7 @@ public class Squishable2D : MonoBehaviour
                     }
                     subject.ApplyDamage(damage * (damageOverTime ? Time.deltaTime : damaged ? 0 : 1) * multiplier);
                     damaged = true;
+                    timer += 1.5f;
                 }
             }
         }
